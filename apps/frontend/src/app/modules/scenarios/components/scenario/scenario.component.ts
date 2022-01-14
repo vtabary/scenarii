80c7261b438782c1ad45ac5ce3a3745569b91481
Vincent Tabary <1731605+vtabary@users.noreply.github.com>
2022-01-14 23:55:34 +0100

    feat(scenario): display status in scenario list

--- apps/frontend/src/app/modules/scenarios/components/scenario/scenario.component.ts
+++ apps/frontend/src/app/modules/scenarios/components/scenario/scenario.component.ts
@@ -1,12 +1,37 @@
-import { Component, Input } from '@angular/core';
-import { IScenario } from '../../../shared/public-api';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  faCheck,
+  faQuestion,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+import { IScenario, ReportsRegistryService } from '../../../shared/public-api';
 
 @Component({
   selector: 'scenarii-scenario',
   templateUrl: './scenario.component.html',
   styleUrls: ['./scenario.component.css'],
 })
-export class ScenarioComponent {
+export class ScenarioComponent implements OnChanges {
   @Input()
   public scenario: IScenario | null = null;
+
+  public faCheck = faCheck;
+  public faTimes = faTimes;
+  public faQuestion = faQuestion;
+  public status: 'success' | 'error' | undefined = undefined;
+
+  constructor(private reports: ReportsRegistryService) {}
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['scenario'] || !this.scenario) {
+      return;
+    }
+
+    const report = this.reports.get(this.scenario.id);
+    if (!report) {
+      return (this.status = undefined);
+    }
+
+    this.status = report.valid ? 'success' : 'error';
+  }
 }
